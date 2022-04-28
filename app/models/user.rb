bf348e7ba817b3af84c4ff56aa2083044f921275
mahiro06 <shishamo06@gmail.com>
2022-04-28 21:03:34 +0900

    userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :name, :profiles, :occupation, :positon, presence: true
+
 end
