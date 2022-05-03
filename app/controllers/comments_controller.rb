42a1d3590ab56d9de9cdd13a4b6a25619054b05c
mahiro06 <shishamo06@gmail.com>
2022-05-03 17:32:15 +0900

    指摘箇所修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,8 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to prototype_path(@comment.prototype)
     else
-      render "prototype/show"
+      @comments = @prototype.comments
+      render "prototypes/show"
     end
   end
 end
